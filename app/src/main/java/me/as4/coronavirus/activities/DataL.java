e4aa2f8234d6a64c1595985b6592c46ad1092876
Andrey Sergeev <wantedmins@gmail.com>
2020-02-29 01:11:01 -0800

    Update
    Signed-off-by: Andrey Sergeev <wantedmins@gmail.com>

---
+++ app/src/main/java/me/as4/coronavirus/activities/DataL.java
@@ -0,0 +1,14 @@
+package me.as4.coronavirus.activities;
+
+class DataL {
+    private double lan;
+    private double lon;
+
+    public void setLan(double lan) {
+        this.lan = lan;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+}
--- app/src/main/java/me/as4/coronavirus/activities/MainActivity.java
+++ app/src/main/java/me/as4/coronavirus/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package me.as4.coronavirus.activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -23,6 +24,14 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.gigamole.navigationtabstrip.NavigationTabStrip;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
@@ -30,12 +39,23 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import me.as4.coronavirus.R;
 import me.as4.coronavirus.fragments.AboutContentActivityFG;
 import me.as4.coronavirus.fragments.MainContentActivityFG;
 import me.as4.coronavirus.models.APIModel;
+import me.as4.coronavirus.models.Feature;
 import me.as4.coronavirus.service.AppCoronaService;
+import me.as4.coronavirus.service.TinyDB;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -48,28 +68,35 @@ public class MainActivity extends AppCompatActivity {
     public static final int progress_bar_type = 0;
     private SharedPreferences mSettings;
     public static final String APP_PREFERENCES = "mysettings";
+    private List<Feature > listmode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Access a Cloud Firestore instance from your Activity
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
         appCoronaService = AppCoronaService.retrofit.create(AppCoronaService.class);
         final Call<APIModel> call = appCoronaService.getData();
-
+        listmode = new ArrayList<>();
         call.enqueue(new Callback<APIModel>() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onResponse(Call<APIModel> call, Response<APIModel> response) {
                 if (response.isSuccessful()) {
+
                     int death = 0;
                     int recover = 0;
                     int confirmed = 0;
                     for (int i = 0; i < response.body().getFeatures().size(); i++) {
+                        Log.d("DATA", "Сountry " + response.body().getFeatures().get(i).getAttributes().getCountryRegion() + ": " + response.body().getFeatures().get(i).getAttributes().getDeaths() );
                         death += response.body().getFeatures().get(i).getAttributes().getDeaths();
                         recover += response.body().getFeatures().get(i).getAttributes().getRecovered();
                         confirmed += response.body().getFeatures().get(i).getAttributes().getConfirmed();
                     }
                     mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
                     SharedPreferences.Editor editor = mSettings.edit();
+
 
                     editor.putInt("deaths", death);
                     editor.apply();
@@ -79,6 +106,59 @@ public class MainActivity extends AppCompatActivity {
                     editor.apply();
 
                     initF();
+                    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+                    LocalDate localDate = LocalDate.now();
+
+
+                    DocumentReference docRef = db.collection("data").document("stat");
+                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+
+                                if (document.exists()) {
+                                    Log.d("DATAT", "DocumentSnapshot data: " + document.getData().get("deaths"));
+
+
+                                } else {
+                                    Log.d("DATA", "No such document");
+                                }
+                            } else {
+                                Log.d("DATA", "get failed with ", task.getException());
+                            }
+                        }
+                    });
+
+
+                    Map<String, Object> docData = new HashMap<>();
+                    Map<String, Object> nestedData = new HashMap<>();
+                    Map<String, Object> nestedData1 = new HashMap<>();
+                    Map<String, Object> nestedData2 = new HashMap<>();
+
+                    nestedData.put(String.valueOf(localDate), confirmed);
+                    nestedData1.put(String.valueOf(localDate), death);
+                    nestedData2.put(String.valueOf(localDate), recover);
+
+                    docData.put("total", nestedData);
+                    docData.put("deaths", nestedData1);
+                    docData.put("recover", nestedData2);
+
+                    db.collection("data").document("stat")
+                            .set(docData)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("data", "DocumentSnapshot successfully written!");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w("DATA", "Error writing document", e);
+                                }
+                            });
+
                 } else {
                     Log.d("DATA", "ERROR");
                 }
